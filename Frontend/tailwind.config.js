307bd36d5b5fb4af5c29adb69cf90e71bdd2e88f
LeeNPham <leenpham@gmail.com>
2024-12-29 00:14:34 -0800

    add flowbite and flowbite-svelte dependencies with custom theme colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,10 +4,27 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+	darkMode: 'selector',
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#E3F2FD', // Lightest blue
+					100: '#BBDEFB',
+					200: '#90CAF9',
+					300: '#64B5F6',
+					400: '#42A5F5',
+					500: '#2196F3', // Main blue
+					600: '#1E88E5',
+					700: '#1976D2',
+					800: '#1565C0',
+					900: '#0D47A1'  // Darkest blue
+				}
+			}
+		}
 	},
 
 	plugins: [typography, forms, containerQueries]
