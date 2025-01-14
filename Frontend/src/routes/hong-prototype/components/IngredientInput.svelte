cba123364fa1e4bbdfd0ccdc38c318d5d0a83887
hongeinh <honganh3179@gmail.com>
2025-01-14 17:46:58 +0700

    Add new recipe input page, reformat/redesign homepage

--- Frontend/src/routes/hong-prototype/components/IngredientInput.svelte
+++ Frontend/src/routes/hong-prototype/components/IngredientInput.svelte
@@ -1,48 +1,39 @@
 <script>
     import { Button, Input } from "flowbite-svelte";
-    let { ingredients=[] } = $props();
+	import { ingredients, ingredientHandler } from "../stores/recipeStore";
 	
-
-	let newIngredient = "";
-
-    function addIngredient() { 
-        if (newIngredient.trim()) {
-            ingredients = [...ingredients, newIngredient];
-            newIngredient = ""
-        }
-    }
-
-    function removeIngredient(index) {
-        ingredients.slice(index, 1);
+	let newIngredient = $state("");
+    function findRecipe() {
+        console.log("Finding recipe");
     }
 </script>
 
 <div>
-    <div class="flex flex-wrap gap-2 mb-4">
-        {#each ingredients as ingredient, i}
-            <div class="flex items-center bg-blue-300 text-blue-900 px-3 py-1 rounded-full h-10 font-semibold font-sans">
-                {ingredient}
-                <Button on:click={() => removeIngredient(i)} 
-                    class="ml-2 bg-blue-300 text-blue-900 hover:text-blue-300 hover:bg-blue-700 font-semibold font-sans">
-                    x
-                </Button>
-            </div>
-        {/each}
-    </div>
-
+    <p class="font-semibold font-sans text-xl text-indigo-500 my-2">What do you want to cook today ?</p>
     <div class="flex items-center gap-2 mb-4">
         <Input 
             type="text"
             bind:value={newIngredient}
             on:keydown={(event) => {
                 if(event.key === "Enter") {
-                    addIngredient();
+                    ingredientHandler.addIngredient(newIngredient.trim());
                     newIngredient = ""
                 }
             }}
-            placeholder="Add new ingredient"
+            placeholder="Put your ingredients here"
             class="px-3 py-1 border rounded-full flex-grow font-sans"
         />
-        <Button on:click={addIngredient} class="bg-blue-300 text-blue-900 px-3 py-1 rounded-full hover:bg-blue-700 hover:text-blue-300 font-semibold font-sans">Add</Button>
+        <Button on:click={findRecipe} class="bg-indigo-300 text-indigo-900 px-3 py-1 rounded-full hover:bg-indigo-700 hover:text-indigo-300 font-semibold font-sans">Search</Button>
+    </div>
+    <div class="flex flex-wrap gap-2 mb-4">
+        {#each $ingredients as ingredient, i}
+            <div class="flex items-center bg-indigo-300 text-indigo-900 px-3 py-1 rounded-full h-10 font-semibold font-sans">
+                {ingredient}
+                <Button on:click={() => ingredientHandler.removeIngredient(i)} 
+                    class="ml-2 bg-indigo-300 text-indigo-900 hover:text-indigo-900 hover:bg-indigo-300 font-semibold font-sans">
+                    x
+                </Button>
+            </div>
+        {/each}
     </div>
 </div>
