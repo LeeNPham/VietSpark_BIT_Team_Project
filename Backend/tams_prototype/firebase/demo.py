29d6b54f9cfd26a4c9eb62b6095ef79697605948
ttp2636 <t477570@gmail.com>
2025-01-29 21:35:26 -0500

    Refactor Firebase demo and main modules: clean up code, remove commented sections, and enhance recipe handling

--- Backend/tams_prototype/firebase/demo.py
+++ Backend/tams_prototype/firebase/demo.py
@@ -2,17 +2,18 @@ import os
 import firebase_admin.auth
 import pyrebase
 import firebase_admin
+import json
+import base64
 from openai import OpenAI
 from fastapi import FastAPI, HTTPException, status
+# from fastapi.responses import JSONResponse
 from google.cloud import firestore
 from dotenv import load_dotenv
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, storage
 from pydantic import BaseModel
 from typing import Optional
 from models import *
 from additionalFucntions import *
-import json
-import base64
 
 load_dotenv()
 # Load Firebase configuration
@@ -40,7 +41,9 @@ db = firestore.Client.from_service_account_json(service_account_path)
 os.remove(service_account_path)
 # do not touch 24-37
 
-
+# firebase_admin.initialize_app(cred, {
+#     'storageBucket': 'chat-app-react-and-firebase.appspot.com'  # Replace with your actual Firebase Storage bucket name
+# })
 firebaseConfig = {
     "apiKey": apiKey,
     "authDomain": authDomain,
@@ -173,12 +176,6 @@ async def update_user_r_a(user_id, recipes, allergies):
             user_collection.document(user_id).update({'allergies': firestore.ArrayUnion(allergies)})
         return {"message": "User data updated successfully."}
     
-    # except auth.AuthError as e:
-    #     print(f"Authentication error: {str(e)}")
-    #     raise HTTPException(status_code=404, detail="User not found.")
-    # except firestore.FirebaseError as e:
-    #     print(f"Firestore error: {str(e)}")
-    #     raise HTTPException(status_code=500, detail="Error updating user data in Firestore.")
     except Exception as e:
         print(f"Unexpected error: {str(e)}")
         raise HTTPException(status_code=500, detail="An unexpected error occurred.")
@@ -192,6 +189,7 @@ async def new_recipe(recipe):
         searchable_ingredient = list(set(searchable_ingredient + ingredient.ingredientName.split())) 
     lower_searchable_ingredient = [item.lower() for item in searchable_ingredient]
     lower_searchable_name = [item.lower() for item in recipe.name.split()]
+
     recipe_data = {
         "name": recipe.name,
         "ingredients": ingredient_data,
@@ -203,11 +201,15 @@ async def new_recipe(recipe):
         "searchable_recipe_name": lower_searchable_name,
         "searchable_ingredient": lower_searchable_ingredient,
     }
+
     if recipe.author != 'string':
         recipe_data['user_id'] = recipe.author
         recipe_data["searchable_recipe_name"].append(recipe.author)
-    recipe_collection.document().set(recipe_data)
-    return {"message": f"Recipe: {recipe.name} added successfully"}
+
+    recipe_doc = recipe_collection.document()
+    recipe_doc.set(recipe_data)
+    recipe_doc.update({"recipe_id": recipe_doc.id})
+    return {"message": f"Recipe: {recipe.name} - ID: {recipe_doc.id} added successfully"}
 
 
 async def get_collection(collection, details):
@@ -261,7 +263,8 @@ async def i_to_r(ingredients):
             recipe_list = list(set(recipe_list) & set(recipe_id))
             if not recipe_list:
                 return "no match"
-        print(recipe_list)
+        # print(recipe_list)
+    
     match_recipe = []
     for recipe_id in recipe_list:
         recipe = await get_document(recipe_collection.document(str(recipe_id)), details=True)
@@ -273,17 +276,17 @@ async def i_to_r(ingredients):
 
 async def GPT_response_to_ingredientS(ingredient):
     OAI_api_key = os.getenv("OAI_API_KEY")
-
     client = OpenAI(
         api_key=OAI_api_key
     )
+
     prompt = f"""
     You are a Vietnamese recipe expert. You are only allowed to respond in the form of a JSON. Time should be in minutes. The JSON should always take the following shape:
     {{
         "name": " ",
         "ingredients": [{{"ingredientName": "ingredientName", "ingredientAmount": "ingredient amount with unit"}}],
         "calories": int,
-        "time": " ",
+        "time": "minutes",
         "servings": int,
         "instructions": ["step 1", "step 2"]
     }}
@@ -293,8 +296,6 @@ async def GPT_response_to_ingredientS(ingredient):
     Respond only with the JSON format as described above.
     """
 
-    # Initialize a message history list with the system prompt
-    # This structure is required for the OpenAI chat model to maintain context.
     messages = [{"role": "system", "content": prompt}]
 
     # Define a function to handle user input and generate a response using OpenAI's API
@@ -307,7 +308,7 @@ async def GPT_response_to_ingredientS(ingredient):
     ChatGPT_reply = response.choices[0].message.content
     recipe_json = json.loads(ChatGPT_reply)
     recipe_json["author"] = "string"
-    print(recipe_json["author"])
+    # print(recipe_json["author"])
     recipe_name = recipe_json['name']
     recipe = RecipeModel.model_validate(recipe_json)
     # recipe["user_id"] = "string"
@@ -317,6 +318,41 @@ async def GPT_response_to_ingredientS(ingredient):
     response_list = [recipe_name, recipe]
 
     return response_list
+
+
+
+
+# async def GPT_image(ingredient):
+#     OAI_api_key = os.getenv("OAI_API_KEY")
+
+
+# async def image_to_storage(file):
+#     bucket = storage.bucket()
+#     # Create a temporary file to store the image
+#     temp_file_path = f"temp_{file.filename}"
+#     # Save the uploaded image to the temporary file
+#     with open(temp_file_path, "wb") as temp_file:
+#         temp_file.write(await file.read())
+    
+#     try:
+#         # Upload the image to Firebase Storage
+#         blob = bucket.blob(f"images/{file.filename}")
+#         blob.upload_from_filename(temp_file_path)
+
+#         # Make the file publicly accessible (optional)
+#         blob.make_public()
+
+#         # Get the public URL of the uploaded image
+#         image_url = blob.public_url
+        
+#         # Clean up the temporary file
+#         os.remove(temp_file_path)
+
+#         return {"message": "Image uploaded successfully", "image_url": image_url}
+
+#     except Exception as e:
+#         os.remove(temp_file_path)
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 
