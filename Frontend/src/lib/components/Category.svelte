d70d548e7cdc8fe4efa96dcc36e2f2ef02d0c376
Mai <60695595+mai-repo@users.noreply.github.com>
2025-02-20 23:46:53 -0500

    Update .gitignore, remove unused images, and enhance component styles and functionality

--- Frontend/src/lib/components/Category.svelte
+++ Frontend/src/lib/components/Category.svelte
@@ -16,9 +16,9 @@
 <div class="flex flex-col p-3 sm:p-3 md:p-5 lg:p-9">
 	<div class="flex flex-wrap justify-center gap-3">
 		{#each $categories as category, i}
-			<category-btn	type="button" class={customStyles.categoryBtn} onclick={() => handleCategoryClick(category)} aria-label="selection button">
-				{category}
-			</category-btn>
+		<button class={customStyles.categoryBtn} onclick={() => handleCategoryClick(category)} aria-label="selection button">
+				<img src="/{category}.png" alt="{category}">
+		</button>
 		{/each}
 	</div>
 </div>
