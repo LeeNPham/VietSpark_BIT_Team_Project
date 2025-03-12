d282490faeebc925af2b76e94366e706045b4beb
hongeinh <honganh3179@gmail.com>
2025-03-11 17:34:00 -0700

    task: MFLP-26 reciple list to horizontal card

--- Frontend/src/lib/components/RecipeList.svelte
+++ Frontend/src/lib/components/RecipeList.svelte
@@ -2,6 +2,7 @@
 	//@ts-nocheck
 	import type { RecipeDTO } from '$lib/types';
 	import { recipeHandler, recipeStore } from '$lib/stores/recipeStore';
+	import { Card } from 'flowbite-svelte';
 
 	export let myRecipes = []; // passed down option property in the case that a user has favorited recipes
 	export let showFavorites = false;
@@ -18,7 +19,7 @@
 		myRecipesData = recipes.filter((recipe) => myRecipes.includes(recipe.recipe_id));
 	} else {
 		myRecipesData = recipes;
-	} 
+	}
 
 	// look into fuze.js for better search functionality, this includes details on how to implement a fuzzy
 	// $: if (searchTerm !== '') {
@@ -37,25 +38,25 @@
 	bind:value={searchTerm}
 /> -->
 
-<div class="mb-4 grid grid-cols-1 gap-2 sm:grid-cols-2 md:p-5 lg:grid-cols-3 xl:grid-cols-4">
+<div class="mb-4 grid grid-cols-1 gap-4 md:p-5 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 place-content-center">
 	{#if myRecipesData}
 		{#each myRecipesData as item (item.recipe_id)}
-			<a
-				class="mb-4 flex w-full items-center gap-4 rounded-lg p-4"
-				href={`/recipe/${item.recipe_id}`}
-			>
-				<div class="flex w-full items-center gap-4">
-					<img src={item.img_url} class="h-20 w-20 rounded-lg object-cover" alt={item.name} />
-					<div class="flex-1">
-						<p class="line-clamp-1">{item.name}</p>
-						<div class="text-secondary-forest space-x-2 text-sm font-medium">
-							<span>{item.time} minutes</span>
-							<span>*</span>
-							<span>{item.numIngredients} ingredients</span>
-						</div>
-					</div>
-				</div>
-			</a>
+			<div class="space-y-4">
+				<Card
+					img={item.img_url}
+					class="m-3 flex-shrink-0 rounded-lg bg-cover bg-center bg-no-repeat shadow-lg mx-auto"
+					href={`/recipe/${item.recipe_id}`}
+					horizontal
+					size="md"
+				>
+					<h5
+						class="mb-3 line-clamp-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
+					>
+						{item.name}
+					</h5>
+					<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-relaxed">{item.time} minutes</p>
+				</Card>
+			</div>
 		{/each}
 	{:else}
 		<div class="flex h-full w-full items-center justify-center">
