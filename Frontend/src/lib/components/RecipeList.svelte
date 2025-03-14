3dd0ed4c3dd020f070f6ef41d96ea7d7b937d893
hongeinh <honganh3179@gmail.com>
2025-03-13 20:39:24 -0700

    Add swipe button to briefly view recipe info

--- Frontend/src/lib/components/RecipeList.svelte
+++ Frontend/src/lib/components/RecipeList.svelte
@@ -6,9 +6,11 @@
 
 	export let myRecipes = []; // passed down option property in the case that a user has favorited recipes
 	export let showFavorites = false;
+
 	let searchTerm = '';
 	let myRecipesData = [];
 	let recipes: RecipeDTO[] | [] = [];
+	let shownDescriptions = {};
 
 	recipeStore.subscribe((store) => {
 		recipes = store.recipes;
@@ -29,6 +31,11 @@
 	// } else {
 	// 	myRecipesData = recipes;
 	// }
+
+	function toggleDescription(recipeId: string) {
+		shownDescriptions[recipeId] = !shownDescriptions[recipeId];
+		shownDescriptions = { ...shownDescriptions };
+	}
 </script>
 
 <!-- <input
@@ -38,23 +45,70 @@
 	bind:value={searchTerm}
 /> -->
 
-<div class="mb-4 grid grid-cols-1 gap-4 md:p-5 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 place-content-center">
+<div
+	class="mb-4 grid grid-cols-1 place-content-center gap-4 sm:grid-cols-2 md:p-5 lg:grid-cols-2 xl:grid-cols-2"
+>
 	{#if myRecipesData}
 		{#each myRecipesData as item (item.recipe_id)}
 			<div class="space-y-4">
 				<Card
 					img={item.img_url}
-					class="m-3 flex-shrink-0 rounded-lg bg-cover bg-center bg-no-repeat shadow-lg mx-auto"
+					class="m-3 mx-auto flex-shrink-0 rounded-lg bg-cover bg-center bg-no-repeat shadow-lg"
 					href={`/recipe/${item.recipe_id}`}
 					horizontal
 					size="md"
 				>
-					<h5
-						class="mb-3 line-clamp-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
-					>
-						{item.name}
-					</h5>
-					<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-relaxed">{item.time} minutes</p>
+					{#if shownDescriptions[item.recipe_id]}
+						<p class="text-md"><strong>⏰ Time:</strong> {item.time} mins</p>
+						<p class="text-md"><strong>🔥 Calories:</strong> {item.calories} kcal</p>
+						<p class="text-md"><strong>🥘 Servings:</strong> {item.servings}</p>
+						<div class="mt-10 flex items-end justify-between">
+							<button
+								class="text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-300 mt-auto flex w-fit items-center"
+								onclick={(event) => {
+									event.preventDefault();
+									toggleDescription(item.recipe_id);
+								}}
+								aria-label="Hide description"
+							>
+								<span class="mr-2">Back</span>
+								<svg class="mr-2 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M15 19l-7-7 7-7"
+									/>
+								</svg>
+							</button>
+						</div>
+					{:else}
+						<h5
+							class="mb-3 line-clamp-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
+						>
+							{item.name}
+						</h5>
+						<div class="mt-10 flex items-end justify-between">
+							<div></div>
+							<button
+								class="text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-300 mt-auto flex w-fit items-center"
+								onclick={(event) => {
+									event.preventDefault();
+									toggleDescription(item.recipe_id);
+								}}
+							>
+								<span class="mr-2">Summary</span>
+								<svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M9 5l7 7-7 7"
+									/>
+								</svg>
+							</button>
+						</div>
+					{/if}
 				</Card>
 			</div>
 		{/each}
